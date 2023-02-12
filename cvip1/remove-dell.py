7dbf32c7d6698369719e387fa64a20e74c720908
EssentialAI <naresh017edu@gmail.com>
2023-02-11 21:13:11 -0500

    rl code changes

--- cvip1/remove-dell.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-os.chdir(r"C:\Users\kumar\Downloads\UB\essentialai\cvip")
-
-if os.path.exists("_build"):
-    shutil.rmtree("_build")
-    print("Deleted _build")
-
-os.system("jb build ../cvip/")
--- cvip1/remove.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-os.chdir(r"C:\Users\nares\Downloads\Fall-2022\cvip-doc\cvip")
-
-if os.path.exists("_build"):
-    shutil.rmtree("_build")
-    print("Deleted _build")
-
-os.system("jb build ../cvip/")
